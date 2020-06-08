44aa8674b6a97b6016726996c12b0fc71af9c781
TMax66 <massimo.tranquillo@gmail.com>
2020-06-08 23:03:30 +0200

    update

--- code.R
+++ code.R
@@ -61,9 +61,41 @@ p6<-lnf %>%
 
 
 
+
 dt %>% 
-  ggplot(aes(x=as.factor(Grgrade), y=MNC))+geom_boxplot()+geom_jitter()
+  ggplot(aes(x=as.factor(Grgrade), col=Micro, fill=Micro))+
+geom_bar()+labs(x="Grado Istologico", y="N.granulomi")
+
+p7<-dt%>% 
+  group_by(Grgrade) %>% 
+  summarise(n=n()) %>% 
+  arrange(desc(n)) %>% 
+  mutate(reg=factor(Grgrade, unique(Grgrade))) %>% 
+  ggplot(aes(x=reg, y=n))+
+  geom_bar(stat="identity", position=position_dodge(), width = 0.6,fill="steelblue")+
+  labs(x="grado istologico", y="N.granulomi")+
+  theme(axis.text=element_text(size=12))+theme_light()+
+  geom_text(aes(label=n), vjust=1.6, color="white",
+            position = position_dodge(0.9), size=3.5)+
+  scale_fill_brewer(palette="Paired")
   
+
+
+p8<-dt%>% 
+  group_by(Grgrade, Micro) %>% 
+  summarise(n=n()) %>% 
+  arrange(n) %>% 
+  mutate(reg=factor(Grgrade, unique(Grgrade))) %>% 
+  ggplot(aes(x=reg, y=n, fill=Micro))+
+  geom_bar(stat="identity", position=position_dodge(), width = 0.6)+
+  labs(x="grado istologico", y="N.granulomi")+
+  theme(axis.text=element_text(size=12))+theme_light()+
+    geom_text(aes(label=n), vjust=1.6, color="white",
+              position = position_dodge(0.9), size=3.5)+
+    scale_fill_brewer(palette="Paired")
+  
+
+(p7+p8)+plot_annotation(tag_levels= 'a') 
 
 dt %>% 
   ggplot(aes(x=as.factor(Grgrade), y=log(NAF+1)))+geom_boxplot() +
@@ -86,5 +118,79 @@ dt %>%
   facet_wrap(~Idlinf)
 
 
-######multilevel model####
+######ml####
 
+#p4 modificato
+
+  dt %>% 
+  select(Idlinf, Micro,totalArea, Grgrade) %>% 
+  group_by(Idlinf, Micro,totalArea,Grgrade) %>% 
+  mutate(ngr=n()) %>% 
+  unique() %>% 
+  ggplot()+
+  geom_density(aes(x=log(ngr), colour=Micro), adjust=1.8)+
+  geom_rug(aes(x = log(ngr),y = 0), position = position_jitter(height = 0))+
+  theme_light()+facet_grid(~Grgrade)
+
+#p5 modificato
+  dt %>% 
+  select(Idlinf, Micro,totalArea, Grgrade) %>% 
+  group_by(Idlinf, Micro,totalArea,Grgrade) %>% 
+  mutate(ngr=n()) %>% 
+  unique() %>% 
+  ggplot(aes(x=Micro, y=log(ngr), color=Micro))+
+  geom_boxplot(width=0.3)+geom_jitter(alpha=0.6, width = 0.15)+
+  theme_light()+facet_grid(~Grgrade)
+  
+  
+#p6 modificato
+  dt %>% 
+    select(Idlinf, Micro,totalArea, Grgrade) %>% 
+    group_by(Idlinf, Micro,totalArea,Grgrade) %>% 
+    mutate(ngr=n()) %>% 
+    unique() %>% 
+    ggplot(aes(x=Micro, y=log(ngr/totalArea), color=Micro))+
+    geom_boxplot(width=0.3)+geom_jitter(alpha=0.6,width = 0.15) +
+    theme_light()+facet_grid(~Grgrade)
+
+#somma delle aree dei singoli granulomi by grade e microbiologia
+  dt %>% 
+    select(Idlinf, Micro,totalArea, Grgrade, Grarea) %>% 
+    group_by(Idlinf, Micro,Grgrade) %>% 
+    summarise(totAgrGrade=sum(Grarea)) %>% 
+    ggplot(aes(x=Micro, y=log(totAgrGrade), color=Micro))+
+    geom_boxplot(width=0.3)+geom_jitter(alpha=0.6,width = 0.15) +
+    theme_light()+facet_grid(~Grgrade)
+  
+  
+  
+ x<- dt %>% 
+    select(Idlinf, Micro,totalArea, Grgrade, Grarea) %>% 
+    group_by(Idlinf, Micro,Grgrade) %>% 
+    summarise(totAgrGrade=sum(Grarea))
+ 
+  y<-dt %>% 
+    select(Idlinf, Micro,totalArea) %>% 
+    group_by(Idlinf, Micro,totalArea) %>% 
+    unique()
+  
+  
+  z<-x %>% 
+    left_join(y, by="Idlinf") 
+  
+  z %>% 
+    ggplot(aes(x=Micro.x, y=log(totAgrGrade/totalArea), color=Micro.x))+
+    geom_boxplot(width=0.3)+geom_jitter(alpha=0.6,width = 0.15) +
+    theme_light()+facet_grid(~Grgrade)
+  
+  
+  
+  ##diametro=2*(sqrt(Area/pi))
+  
+  dati<-dt %>% 
+    group_by(Idlinf) %>% 
+    mutate(diametro=2*sqrt(totalArea/3.14))
+    
+  
+
+  
