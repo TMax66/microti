d305a58bddd4651b7da9421fe5b37ad20c080714
TMax66 <massimo.tranquillo@gmail.com>
2020-06-15 00:07:55 +0200

    update

--- code.R
+++ code.R
@@ -157,11 +157,32 @@ dt %>%
   dt %>% 
     select(Idlinf, Micro,totalArea, Grgrade, Grarea) %>% 
     group_by(Idlinf, Micro,Grgrade) %>% 
-    summarise(totAgrGrade=sum(Grarea)) %>% 
+  
     ggplot(aes(x=Micro, y=log(totAgrGrade), color=Micro))+
     geom_boxplot(width=0.3)+geom_jitter(alpha=0.6,width = 0.15) +
-    theme_light()+facet_grid(~Grgrade)
+    theme_light()+facet_grid(~Grgrade)+labs(title="Distribuzione della somma delle aree dei granulomi in base a status microbiologico e grado istologico")
   
+  
+  
+  dt %>% 
+    select(Idlinf, Micro,totalArea, Grgrade, Grarea) %>% 
+    group_by(Idlinf, Micro,Grgrade) %>% 
+    mutate(nArea=Grarea/totalArea) %>% 
+    summarise(SumnArea=sum(nArea)) %>% 
+    ggplot(aes(x=Micro, y=log(SumnArea), color=Micro))+
+    geom_boxplot(width=0.3)+geom_jitter(alpha=0.6,width = 0.15) +
+    theme_light()+facet_grid(~Grgrade)+labs(title="Distribuzione della somma normalizzata delle singole aree dei granulomi in base a status microbiologico e grado istologico")
+  
+  
+  
+  
+  dt %>% 
+    select(Grgrade, Grarea) %>% 
+    group_by(Grgrade) %>% 
+    summarise(a=mean(Grarea),
+              min=min(Grarea),
+              max=max(Grarea)) %>% 
+    ggplot(aes(x=Grgrade, y=Grarea))+geom_boxplot(width=0.3)
   
   
  x<- dt %>% 
@@ -191,6 +212,187 @@ dt %>%
     group_by(Idlinf) %>% 
     mutate(diametro=2*sqrt(totalArea/3.14))
     
+################################################################################
+  
+dt %>% 
+    select(Idlinf,Grgrade, Micro, MNC, NAF) %>% 
+    group_by(Idlinf, Grgrade, Micro) %>% 
+    summarise(mnc=mean(log(MNC+1)),
+              zhil=mean(log(NAF+1))) %>% 
+    ggplot(aes(x=as.factor(Grgrade), y=mnc))+
+    geom_boxplot(width=0.3, )+geom_jitter(alpha=0.6,width = 0.15) +
+    theme_light()+facet_grid(~Micro)
+      
+
+  dt %>% 
+    select(Idlinf,Grgrade, Micro, MNC, NAF) %>% 
+    group_by(Idlinf, Grgrade, Micro) %>% 
+    summarise(mnc=mean(log(MNC+1)),
+              naf=mean(log(NAF+1))) %>% 
+    ggplot(aes(x=as.factor(Grgrade), y=naf))+
+    geom_boxplot(width=0.3, )+geom_jitter(alpha=0.6,width = 0.15) +
+    theme_light()+facet_grid(~Micro)
+  
+  
+  
+  
+  dt %>% 
+    select(Idlinf, Micro,totalArea, Grgrade) %>% 
+    group_by(Idlinf, Micro,totalArea,Grgrade) %>% 
+    mutate(ngr=n()) %>% 
+    mutate(grade=factor(Grgrade)) %>% 
+    unique() %>% 
+    ggplot()+
+    geom_density(aes(x=log(ngr), colour=grade), adjust=1.8)+
+    geom_rug(aes(x = log(ngr),y = 0), position = position_jitter(height = 0))+
+    theme_light()+facet_grid(~Micro)
+  
+  
+  
+  p5m<- dt %>%
+    select(Idlinf, Micro,totalArea, Grgrade) %>%
+    group_by(Idlinf, Micro,totalArea,Grgrade) %>%
+    mutate(ngr=n()) %>%
+    mutate(grade=factor(Grgrade)) %>% 
+    unique() %>%
+    ggplot(aes(x=Micro, y=log(ngr), color=Micro))+
+    geom_boxplot(width=0.3)+geom_jitter(alpha=0.6, width = 0.15)+
+    theme_light()+facet_grid(~Grgrade)
+  
+  
+  dt %>% 
+    select(Idlinf, Micro,totalArea, Grgrade, Grarea) %>% 
+    group_by(Idlinf, Micro,Grgrade) %>% 
+    mutate(nArea=Grarea/totalArea) %>% 
+    summarise(SumnArea=sum(nArea)) %>% 
+    ggplot(aes(x=as.factor(Grgrade), y=log(SumnArea)))+
+    geom_boxplot(width=0.3)+geom_jitter(alpha=0.6,width = 0.15) + 
+    theme_light()+facet_grid(~Micro)+labs(x="grado istologico",caption="Distribuzione della somma normalizzata delle singole aree dei granulomi in base a status microbiologico e grado istologico") 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+#################
+  
+  
+dt %>% 
+    group_by(Micro,Grgrade) %>% 
+    summarise(ngr=n()) %>% 
+    ggplot(aes(x=Grgrade, y=ngr)) + 
+    geom_bar(stat="identity", position=position_dodge(), width = 0.6, fill="steelblue")+
+    facet_wrap(~Micro)
+    
+  
+dt %>% 
+  mutate(MNCq=ifelse(MNC==0, 0, 1)) %>% 
+  group_by(Idlinf, Grgrade, Micro) %>% 
+  summarise(mncz=1-mean(MNCq)) %>% 
+  ggplot(aes(x=Grgrade, y=mncz)) +
+  geom_bar(stat="identity") +
+  facet_wrap(~Micro)
+  
+
+
+  
+  
+  
+library(brms)
+  
+ 
+options(mc.cores=parallel::detectCores())
   
 
   
+nullmdl <- brm(Grgrade ~ (1|Idlinf), data=dt,
+               family=cumulative("logit"), cores = 4, iter = 4000)
+  
+  
+  
+modM <- brm(Grgrade ~ Micro+(1|Idlinf), data=dt, family=cumulative("logit"), threshold="flexible")
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+
+# library(rethinking)
+# simplehist(dt$Grgrade, xlim=c(1,4), xlab="Grado")  
+# 
+# pr_k<-table(dt$Grgrade)/nrow(dt)
+# cum_pr_k<-cumsum(pr_k)
+# plot(1:4, cum_pr_k, type="b",xlab="Grado")
+# 
+# logit<-function(x) log(x/(1-x))
+# lco<-logit(cum_pr_k)
+# 
+# plot(1:4, lco, type="b",xlab="Grado")
+# 
+# 
+# m1.0<-ulam(
+#   alist(
+#     Gr~dordlogit(0, cutpoints),
+#     cutpoints~dnorm(0,1.5)
+#   ), data=list(Gr=dt$Grgrade), chains=4,cores=4)
+# 
+# 
+# dat<-list(Gr=dt$Grgrade,
+#           M=as.integer(as.factor(dt$Micro))-1)
+# 
+# 
+# m1.1<-ulam(
+#   alist(
+#     Gr~dordlogit(phi, cutpoints),
+#     phi<-bM*M,
+#     bM~dnorm(0,0.5),
+#     cutpoints~dnorm(0,1.5)
+#   ), data=dat, chains=4,cores=4)
+# 
+# 
+# 
+# 
+# cutpoints <- m1.1 %>%
+#   recover_types(dat) %>%
+#   spread_draws(cutpoints[Gr])
+# 
+# # define the last cutpoint
+# last_cutpoint <- tibble(
+#   .draw <- 1:max(cutpoints$.draw),
+#   Gr <- "4",
+#   cutpoints <- Inf
+# )
+# 
+# 
+# cutpoints = bind_rows(cutpoints, last_cutpoint) %>%
+#   # define the previous cutpoint (cutpoint_{j-1})
+#   group_by(.draw) %>%
+#   arrange(Gr) %>%
+#   mutate(prev_cutpoint = lag(cutpoints, default = -Inf))
+# 
+# cutpoints %>%
+#   group_by(Gr) %>%
+#   median_qi(cutpoints, prev_cutpoint)
+# 
+# 
+# fitted_cGr_probs = dat %>%
+#   data_grid(mpg = seq_range(mpg, n = 101)) %>%
+#   add_fitted_draws(m_cyl) %>%
+#   inner_join(cutpoints, by = ".draw") %>%
+#   mutate(`P(cyl | mpg)` =
+#            # this part is logit^-1(cutpoint_j - beta*x) - logit^-1(cutpoint_{j-1} - beta*x)
+#            plogis(cutpoint - .value) - plogis(prev_cutpoint - .value)
+#   )
+# 
+# fitted_cyl_probs %>%
+#   head(10)
